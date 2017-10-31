a8f0fe8299b5033d9a3e706440b54a1e260f5e08
TanmayTapar <tanmaytapar@gmail.com>
2017-10-31 13:55:35 -0400

    First Commit Project created!

---
+++ src/main/java/Java8_Features/test/java8/practice/App.java
@@ -0,0 +1,13 @@
+package Java8_Features.test.java8.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
